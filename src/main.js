d9499a52e99888885ec9be025537f53fcd291fab
储怡然 <252964202@qq.com>
2021-01-21 18:36:18 +0800

    '2021-01-21'

--- src/main.js
+++ src/main.js
@@ -1,12 +1,18 @@
 import Vue from 'vue'
 import App from './App.vue'
-import router from './router'
-import store from './store'
+import router  from "@/router";
+//引入并注册bootstrap-vue
+import BootstrapVue from "bootstrap-vue";
+Vue.use(BootstrapVue)
+//引入bootstrap-vue与bootstrap样式文件
+import  'bootstrap/dist/css/bootstrap.css'
+import  'bootstrap-vue/dist/bootstrap-vue.css'
 
+import  '../src/assets/css/html.css'
 Vue.config.productionTip = false
 
 new Vue({
   router,
-  store,
+  // store,
   render: h => h(App)
 }).$mount('#app')
